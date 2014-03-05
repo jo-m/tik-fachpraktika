6ac087def2f9be157ad5270c5885bd84b7d64d92
Jonathan Mueller <j.mueller@apoveda.ch>
2014-03-05 21:24:05 +0100

    ex 2.1

---
+++ 2_threads/makethreads.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <string.h>
+#include <pthread.h>
+#include <errno.h>
+#include <signal.h>
+
+// gcc -Wall -o makethreads makethreads.c && ./makethreads
+
+void handle_sigint(int signum)
+{
+  printf("terminating.\n");
+  exit(EXIT_SUCCESS);
+}
+
+void err_exit(const int c) {
+  printf("Invalid or not enough arguments received (Error code: %d)\n", errno);
+  puts("    Usage: ./makethreads <numthreads>");
+  exit(1);
+}
+
+void *hello_thread(void *arg) {
+  while(1) {
+    printf("Thread <%d>: Hello World!\n", (int)arg);
+    sleep(1);
+  }
+}
+
+int main(const int argc, char const **argv) {
+  if(argc < 2) err_exit(1);
+  const long numthreads = strtol(argv[1], NULL, 10);
+  if(0 != errno) err_exit(2);
+  pthread_t threadinfo;
+
+  for(int i = 0; i < numthreads; i++)
+  pthread_create(&threadinfo, NULL, hello_thread, (void *)i);
+
+  signal(SIGINT, handle_sigint);
+
+  while(1)
+    sleep(1);
+
+  return EXIT_SUCCESS;
+}
