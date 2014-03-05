99fbcf3819ca0c63e95c537c256fea98ffba5e4b
Jonathan Mueller <j.mueller@apoveda.ch>
2014-03-05 14:43:37 +0100

    ex 2.1

--- 1_intro/net.c
+++ 1_intro/net.c
@@ -1,7 +1,7 @@
-/* gcc -Wall net.c */
-
 #include <stdio.h>
 #include <stdint.h>
+
+// gcc -Wall net.c -o net && ./net
 
 int main(void)
 {
@@ -9,7 +9,9 @@ int main(void)
 	uint8_t sub[] = {255, 255, 240,   0};
 	uint8_t net[4];
 
-	/* ... */
+  for(int i = 0; i < 4; i++) {
+    net[i] = add[i] & sub[i];
+  }
 
 	printf("net address: %u.%u.%u.%u\n", net[0], net[1], net[2], net[3]);
 
