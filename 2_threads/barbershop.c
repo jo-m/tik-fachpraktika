1261285f621d53dc1162eac9b09b830ad0fbd8ff
Jonathan Mueller <j.mueller@apoveda.ch>
2014-03-06 10:29:19 +0100

    solution for barbershop

--- 2_threads/barbershop.c
+++ 2_threads/barbershop.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -10,10 +11,9 @@
 struct shop_thread_priv_data;
 
 struct shop {
-  sem_t customers_in_shop;
   int customers_shop_max;
-  int customers_curr;
-  sem_t sofa;
+  sem_t customers_in_shop;
+  sem_t customers_on_sofa;
   sem_t cashdesk;
   sem_t cut_customer_ready;
   sem_t cut_barber_ready;
@@ -57,6 +57,12 @@ static void xfree(void *ptr)
   free(ptr);
 }
 
+int xsem_getvalue(sem_t *sem) {
+  int val;
+  sem_getvalue(sem, &val);
+  return val;
+}
+
 void signal_quit(int signum)
 {
   pthread_exit(0);
@@ -73,25 +79,29 @@ static void *barber_main(void *arg)
 
   /* Do the barber job */
   while (1) {
-    /* I want to cut hair */ 
-    /* ... */
+    /* I want to cut hair */
+    sem_post(&s->cut_barber_ready);
+
     /* I am waiting for a client */
     printf("barber %d waiting for customer\n", std->thread_num);
-    /* ... */
+    sem_wait(&s->cut_customer_ready);
+
     /* I am cutting hair */
     printf("barber %d cutting hair\n", std->thread_num);
-    sleep(3);
+    sleep(1);
     /* Tell customer we're done */
-    /* ... */
+    sem_post(&s->cut_done);
+
     /* Wait for cash desk until free */
-    /* ... */
+    sem_wait(&s->cashdesk);
     printf("barber %d went to cash desk, waits for customer "
            "to pay\n", std->thread_num);
     /* Make the payment */
-    /* ... */
+    sem_post(&s->pay_barber_ready);
+    sem_wait(&s->pay_customer_ready);
     printf("barber %d got payment\n", std->thread_num);
     /* Free cash desk */
-    /* ... */
+    sem_post(&s->cashdesk);
   }
 
   printf("barber %d died\n", std->thread_num);
@@ -107,32 +117,37 @@ static void *customer_main(void *arg)
   s = std->shop;
 
   /* Try to enter the shop */
-  /* ... */
+  while(-1 == sem_trywait(&s->customers_in_shop)) {
+    printf("customer %d going for a drink\n", std->thread_num);
+    sleep(1);
+  }
 
   /* Wait on the sofa */
-  /* ... */
+  sem_wait(&s->customers_on_sofa);
   printf("customer %d sitting on sofa, waits for barber\n",
          std->thread_num);
 
   /* Wait for a barber */
-  /* ... */
+  sem_post(&s->cut_customer_ready);
+  sem_wait(&s->cut_barber_ready);
 
   /* Leave the sofa */
-  /* ... */
+  sem_post(&s->customers_on_sofa);
   printf("customer %d went up from sofa\n", std->thread_num);
 
   /* Wait until barber is done */
-  /* ... */
+  sem_wait(&s->cut_done);
 
   /* Pay for the hair cut */
-  /* ... */
+  sem_post(&s->pay_customer_ready);
+  sem_wait(&s->pay_barber_ready);
   printf("customer %d payed money\n", std->thread_num);
 
   /* Leave the shop */
-  /* ... */
+  sem_post(&s->customers_in_shop);
 
   printf("customer %d left the shop, %d still in the shop\n",
-         std->thread_num, s->customers_curr);
+         std->thread_num, s->customers_shop_max - xsem_getvalue(&s->customers_in_shop));
 
   printf("customer %d died\n", std->thread_num);
   pthread_exit(0);
@@ -145,8 +160,8 @@ static void shop_create(struct shop *s)
   s->customers = xmalloc(s->customers_num * sizeof(*(s->customers)));
   s->customers_priv = xmalloc(s->customers_num * sizeof(*(s->customers_priv)));
 
-  sem_init(&s->customers_in_shop, 0, 1);
-  sem_init(&s->sofa, 0, 4);
+  sem_init(&s->customers_in_shop, 0, s->customers_shop_max);
+  sem_init(&s->customers_on_sofa, 0, 4);
   sem_init(&s->cashdesk, 0, 1);
 
   sem_init(&s->cut_customer_ready, 0, 0);
@@ -196,7 +211,7 @@ static void shop_cleanup(struct shop *s)
   xfree(s->barbers_priv);
 
   sem_destroy(&s->customers_in_shop);
-  sem_destroy(&s->sofa);
+  sem_destroy(&s->customers_on_sofa);
   sem_destroy(&s->cashdesk);
 
   sem_destroy(&s->cut_customer_ready);
@@ -212,7 +227,7 @@ int main(int argc, char **argv)
 
   memset(&s, 0, sizeof(s));
   s.barbers_num = 4;
-  s.customers_num = 20;
+  s.customers_num = 40;
   s.customers_shop_max = 20;
 
   shop_create(&s);
