3f786cb0599dc7dc2aec18c771121ebf7eb3be45
Jonathan Mueller <j.mueller@apoveda.ch>
2014-03-06 15:21:26 +0000

    begin with ex 5

---
+++ 5_filesys/tikdb.c
@@ -0,0 +1,257 @@
+/* Implementation is lame, but not targeted for a high amount of
+ * key-vals anyway */
+
+// gcc -o tikdb tikdb.c
+
+#include <sys/sendfile.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+
+#ifndef min
+# define min(a, b)                         \
+	({                                 \
+		typeof (a) _a = (a);       \
+		typeof (b) _b = (b);       \
+		_a < _b ? _a : _b;         \
+	})
+#endif /* min */
+
+#define TIKDB_MAGIC		0xbeefee
+#define TIKDB_VERSION_MAJ	1
+#define TIKDB_VERSION_MIN	0
+
+struct tikdb_filehdr {
+	uint32_t magic;
+	uint16_t version_major;
+	uint16_t version_minor;
+};
+
+struct tikdb_datahdr {
+	char key[32];
+	time_t ts_r;
+	time_t ts_w;
+	uint32_t len;
+};
+
+static inline void die(void)
+{
+	exit(1);
+}
+
+static void panic(const char *serror)
+{
+	printf("%s", serror);
+	die();
+}
+
+static void *xmalloc(size_t size)
+{
+	void *ptr;
+	if (size == 0)
+		panic("Size is 0!\n");
+	ptr = malloc(size);
+	if (!ptr)
+		panic("No mem left!\n");
+	return ptr;
+}
+
+static void xfree(void *ptr)
+{
+	if (ptr == NULL)
+		panic("Got Null-Pointer!\n");
+	free(ptr);
+}
+
+static int __create_db(char *name)
+{
+	int fd, ret;
+	struct tikdb_filehdr db;
+	fd = open(name, O_RDWR | O_CREAT | O_TRUNC, 0644);
+	if (fd < 0)
+		panic("Cannot open file!\n");
+	memset(&db, 0, sizeof(db));
+	db.magic = TIKDB_MAGIC;
+	db.version_major = TIKDB_VERSION_MAJ;
+	db.version_minor = TIKDB_VERSION_MIN;
+	ret = write(fd, &db, sizeof(db));
+	if (ret != sizeof(db))
+		panic("Could not write db\n");
+	return fd;
+}
+
+static void create_db(char *name)
+{
+	close(__create_db(name));
+	die();
+}
+
+static void check_db_hdr(int fd)
+{
+	int ret;
+	struct tikdb_filehdr db;
+	memset(&db, 0, sizeof(db));
+	ret = read(fd, &db, sizeof(db));
+	if (ret != sizeof(db))
+		panic("Could not read db\n");
+	if (db.magic != TIKDB_MAGIC)
+		panic("Wrong magic number!\n");
+	if (db.version_major != TIKDB_VERSION_MAJ)
+		panic("Wrong version major number!\n");
+	if (db.version_minor != TIKDB_VERSION_MIN)
+		panic("Wrong version minor number!\n");
+}
+
+static void __add_key_val(char *db, char *key, char *val, size_t vlen,
+			  uint8_t *db_pay, size_t db_len)
+{
+	int ret, written = 0, fd = __create_db(db);
+	off_t off = 0;
+	while (off < db_len) {
+		struct tikdb_datahdr *hdr;
+		hdr = (struct tikdb_datahdr *) (db_pay + off);
+		if (!written &&
+		    !memcmp(hdr->key, key, min(strlen(key),
+			    sizeof(hdr->key) - 1))) {
+			struct tikdb_datahdr thdr;
+			memset(&thdr, 0, sizeof(thdr));
+			memcpy(thdr.key, key, min(strlen(key),
+			       sizeof(thdr.key) - 1));
+			thdr.len = vlen;
+			thdr.ts_r = thdr.ts_w = time(NULL);
+			ret = write(fd, &thdr, sizeof(thdr));
+			if (ret != sizeof(thdr))
+				panic("Writing to db .. corrupted!\n");
+			ret = write(fd, val, vlen);
+			if (ret != vlen)
+				panic("Writing to db .. corrupted!\n");
+			written = 1;
+		} else {
+			ret = write(fd, hdr, sizeof(*hdr));
+			if (ret != sizeof(*hdr))
+				panic("Writing to db .. corrupted!\n");
+			ret = write(fd, db_pay + off + sizeof(*hdr), hdr->len);
+			if (ret != hdr->len)
+				panic("Writing to db .. corrupted!\n");
+		}
+		off += sizeof(struct tikdb_datahdr);
+		off += hdr->len;
+	}
+	if (!written) {
+		struct tikdb_datahdr thdr;
+		memset(&thdr, 0, sizeof(thdr));
+		memcpy(thdr.key, key, min(strlen(key), sizeof(thdr.key) - 1));
+		thdr.len = vlen;
+		thdr.ts_r = thdr.ts_w = time(NULL);
+		ret = write(fd, &thdr, sizeof(thdr));
+		if (ret != sizeof(thdr))
+			panic("Writing to db .. corrupted!\n");
+		ret = write(fd, val, vlen);
+		if (ret != vlen)
+			panic("Writing to db .. corrupted!\n");
+	}
+	fsync(fd);
+	close(fd);
+}
+
+static void add_key_val(char *db, char *key, char *val, size_t vlen)
+{
+	int fd, ret;
+	struct stat buff;
+	uint8_t *db_pay;
+	size_t db_len;
+	fd = open(db, O_RDWR);
+	if (fd < 0)
+		panic("Cannot open file!\n");
+	check_db_hdr(fd);
+	ret = fstat(fd, &buff);
+	if (ret < 0)
+		panic("Cannot fstat file!\n");
+	db_len = buff.st_size - sizeof(struct tikdb_filehdr);
+	if (db_len > 0) {
+		db_pay = xmalloc(db_len);
+		ret = read(fd, db_pay, db_len);
+		if (ret != db_len)
+			panic("Cannot read out db!\n");
+		close(fd);
+		__add_key_val(db, key, val, vlen, db_pay, db_len);
+		xfree(db_pay);
+	} else {
+		struct tikdb_datahdr hdr;
+		memset(&hdr, 0, sizeof(hdr));
+		memcpy(hdr.key, key, min(strlen(key), sizeof(hdr.key) - 1));
+		hdr.len = vlen;
+		hdr.ts_r = hdr.ts_w = time(NULL);
+		ret = write(fd, &hdr, sizeof(hdr));
+		if (ret != sizeof(hdr))
+			panic("Writing to db .. corrupted!\n");
+		ret = write(fd, val, vlen);
+		if (ret != vlen)
+			panic("Writing to db .. corrupted!\n");
+		fsync(fd);
+		close(fd);
+	}
+	die();
+}
+
+static void get_key_val(char *db, char *key)
+{
+	int fd, ret;
+	struct tikdb_datahdr hdr;
+	fd = open(db, O_RDONLY);
+	if (fd < 0)
+		panic("Cannot open file!\n");
+	check_db_hdr(fd);
+	while (read(fd, &hdr, sizeof(hdr)) > 0) {
+		if (!memcmp(hdr.key, key, min(strlen(key),
+			    sizeof(hdr.key) - 1))) {
+			char *buff = xmalloc(hdr.len);
+			ret = read(fd, buff, hdr.len);
+			if (ret != hdr.len)
+				panic("Cannnot read value!\n");
+			printf("%s", buff);
+			fflush(stdout);
+			xfree(buff);
+#if 0
+			ret = sendfile(0, fd, 0, hdr.len);
+			if (ret != hdr.len)
+				panic("Cannnot read value!\n");
+			fflush(stdout);
+#endif
+			break;
+		} else {
+			lseek(fd, hdr.len, SEEK_CUR);
+		}
+		memset(&hdr, 0, sizeof(hdr));
+	}
+	close(fd);
+	die();
+}
+
+int main(int argc, char**argv)
+{
+	if (argc < 2)
+		panic("Too few arguments!\n");
+	if (!strcmp(argv[1], "create") && argc == 3)
+		create_db(argv[2]);
+	if (!strcmp(argv[1], "get") && argc == 4)
+		get_key_val(argv[2], argv[3]);
+	if (!strcmp(argv[1], "add") && argc == 5)
+		add_key_val(argv[2], argv[3], argv[4], strlen(argv[4]));
+	printf("Usage i.e.:\n");
+	printf("   tikdb create todo.db\n");
+	printf(" Add key-value:\n");
+	printf("   tikdb add todo.db \"25.02.2012\" \"Watch new Dr Who episodes\"\n");
+	printf(" Update key-value:\n");
+	printf("   tikdb add todo.db \"25.02.2012\" \"Watch old Dr Who episodes\"\n");
+	printf(" Retrival:\n");
+	printf("   tikdb get todo.db \"25.02.2012\"\n");
+	printf("Note: max key size is 31 ASCII chars\n");
+	return 0;
+}
