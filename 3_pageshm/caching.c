2e3ffe1ee128b9b4460b3516df27f8d754bd6eb9
Jonathan Mueller <j.mueller@apoveda.ch>
2014-03-06 09:34:54 +0000

    beginning ex part 3

---
+++ 3_pageshm/caching.c
@@ -0,0 +1,61 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+// gcc -Wall -O2 caching.c -o caching && ./perf stat ./caching 0
+
+static void f(char **addr, size_t x, size_t y)
+{
+	/* Traverse data, method 1 */
+	/* ... */
+}
+
+static void g(char **addr, size_t x, size_t y)
+{
+	/* Traverse data, method 2 */
+	/* ... */
+}
+
+int main(int argc, char **argv)
+{
+	char **addr;
+	size_t x, y;
+	int i;
+
+	if (argc != 2) {
+		printf("Usage: ./a.out <test-nr:0|1>\n");
+		return -EIO;
+	}
+
+	x = y = sysconf(_SC_PAGE_SIZE) * 5;
+	addr = valloc(x * sizeof(char *));
+	if (addr == NULL)
+		return -ENOMEM;
+
+	for (i = 0; i < x; i++) {
+		addr[i] = malloc(y * sizeof(char));
+		if (addr[i] == NULL)
+			return -ENOMEM;
+	}
+
+	switch (atoi(argv[1])) {
+	default:
+	case 0:
+		f(addr, x, y);
+		break;
+	case 1:
+		g(addr, x, y);
+		break;
+	}
+
+
+	for (i = 0; i < x; i++)
+		free(addr[i]);
+	free(addr);
+	return 0;
+}
---
+++ 3_pageshm/consumer.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#define MAX_STR_LEN	4096
+
+static void panic(const char* serror)
+{
+	fprintf(stderr, "%s:", serror);
+	perror("");
+	exit(1);
+}
+
+static void test_data(int *data, size_t len)
+{
+	int i;
+	off_t offset = 0;
+	char argstr[MAX_STR_LEN];
+	char finstr[MAX_STR_LEN + 32];
+
+	if (len >= (MAX_STR_LEN / 3) - 1)
+		panic("No mem left!\n");
+
+	memset(argstr, 0, sizeof(argstr));
+	memset(finstr, 0, sizeof(finstr));
+
+	for (i = 0; i < len; ++i) {
+		if (data[i] < 0 || data[i] >= 100)
+			continue;
+		offset += sprintf(argstr + offset, "%2d ", data[i]);
+	}
+
+	/* Okay here since we cannot run into an buffer overflow */
+	strcpy(finstr, "./tester ");
+	strcat(finstr, argstr);
+	system(finstr);
+}
+
+int main(void)
+{
+	key_t key;
+	const int SIGNAL = -4;
+	int shmid, *shared_data, read[27], i, ret;
+
+	/* Note: always check the error values and implement proper
+	 * error handling! */
+
+	/* Make the key */
+	/* ... */
+	/* Connect to the segment (size is size of one integer) */
+	/* ... */
+	/* Attach to the segment */
+	/* ... */
+	printf("Consumer starts\n");
+	for (i = 0; i < sizeof(read) / sizeof(read[0]); ++i) {
+		/* Read value */
+		/* ... */
+		printf("Consumer read: %d\n", read[i]);
+		/* Notify producer that data has been read */
+		/* ... */
+		/* Wait for the next value */
+		/* ... */
+
+	}
+	/* Detach from segment */
+	/* ... */
+
+	test_data(read, sizeof(read) / sizeof(read[0]));
+	return 0;
+}
